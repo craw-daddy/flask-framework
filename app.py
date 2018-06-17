69b7ebd75159c256150282daf1054c903ebe3674
Russell Martin <russell@martinandbrown.com>
2018-06-17 23:05:36 +0100

    Checkbox functionality

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def graph():
     if request.method == 'POST':
        close = open = adj_open = adj_close = False
        stock = request.form['ticker']
-       if request.form.get("close"):
+       if request.form["close"]:
           close = True
        if request.form.get("open"):
           open = True
